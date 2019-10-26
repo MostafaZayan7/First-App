d46c3f851c6cd2de072cf119c333affc7971101e
DevMostafaZayan <mostafazayan@gmail.com>
2019-10-26 03:00:02 +0200

    Component and template clean up

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -28,9 +28,7 @@ export class UsersComponent implements OnInit {
             city: 'nasr city',
             state: 'cairo'
           },
-          image: 'http://lorempixel.com/600/600/people/3',
           isActive: true,
-          balance:  100,
           registered: new Date('01/02/2018 08:30:00')
         },
         {
@@ -42,8 +40,7 @@ export class UsersComponent implements OnInit {
             city: 'nasr city',
             state: 'cairo'
           },
-          image: 'http://lorempixel.com/600/600/people/2',
-          balance:  200,
+
           registered: new Date('01/03/2017 05:30:00')
         },
         {
@@ -55,36 +52,21 @@ export class UsersComponent implements OnInit {
             city: 'nasr city',
             state: 'cairo'
           },
-          image: 'http://lorempixel.com/600/600/people/1',
-          balance:  300,
+
           registered: new Date('03/05/2016 07:20:00')
         }
       ];
 
       this.loaded = true;
 
-      this.setCurrentClasses();
-      this.setCurrentStyle();
 
-      this.addUser({
-      firstName : 'ali',
-      lastName : 'zayan'
-     });
+
   }
 
   addUser(user: User) {
     this.users.push(user);
   }
 
-  setCurrentClasses() {
-    this.currentClasses = {
-      'btn-success': this.enableAdd
-    };
-  }
-  setCurrentStyle() {
-    this.currentStyle = {
-      'padding-top' : this.showExtend ? '0' : '140px'
-    };
-  }
+
 
 }
